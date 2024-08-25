47e806b047507644769261950aacd063993d3de3
adrees-ali <109959486+adreesali@users.noreply.github.com>
2024-08-25 18:21:09 +0500

    Update types.ts

--- lifecare/src/types.ts
+++ lifecare/src/types.ts
@@ -1,12 +1,21 @@
 export interface ProductType {
-    id: number;
-    title: string;
-    price: number;
-    imageUrl: string;
+  id: number;
+  title: string;
+  price: number;
+  halfPrice?: number;  // Optional prowperty for half price
+  imageUrl: string;
+  quantity: number;
+  category: string;
+}
+
+
+  
+  export interface CartItemType extends ProductType {
     quantity: number;
   }
   
-  
-  export interface CartItemType extends ProductType {
+
+  export interface CartItemType {
+    id: number;
     quantity: number;
   }
